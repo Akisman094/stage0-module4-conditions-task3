20d3197ab12696d18695981e3b60f9b87039a995
Akmal Ismoilov <master80098@gmail.com>
2022-07-28 13:38:33 +0500

    Completed tasks!

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,6 +1,12 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.time.YearMonth;
+
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if(month > 12 || month < 1)
+            System.out.println("wrong number!");
+        else
+            System.out.println(YearMonth.of(2022, month).lengthOfMonth());
     }
 }
